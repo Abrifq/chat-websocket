d68fcaff59c1b583c5c9cb2eb9aa747f7a4a6120
Arda Aydın <arda.aydin@operationsilkscarf.com>
2021-01-28 22:23:30 +0300

    Added a "Not Found" for favicon requests.

--- http.js
+++ http.js
@@ -1,7 +1,8 @@
 const port = require("process").env.PORT || 8080;
 const http = require("http");
 const fs = require("fs");
-const server = http.createServer((_incoming, outgoing) => {
+const server = http.createServer((incoming, outgoing) => {
+    if (incoming.url === "/favicon.ico") { outgoing.writeHead(404, "Not Found"); outgoing.end(); return; }
     outgoing.writeHead(200, "OK", { "Content-Type": "text/html; charset=utf-8" });
     fs.createReadStream("./page.html",
         { encoding: "utf8", mode: fs.constants.O_RDONLY, autoClose: true }
